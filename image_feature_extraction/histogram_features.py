564f9cdae8462b23d8dcda2942a52a58667c7499
mehdiyousefzadeh <47357813+mehdiyousefzadeh@users.noreply.github.com>
2024-06-04 17:42:58 +0330

    Create histogram_features.py

---
+++ image_feature_extraction/histogram_features.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+def hog_features(image):
+    win_size = (64, 64)
+    block_size = (16, 16)
+    block_stride = (8, 8)
+    cell_size = (8, 8)
+    nbins = 9
+    hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
+    h = hog.compute(image)
+    return h
+
+def color_histogram(image, bins=(8, 8, 8)):
+    hist = cv2.calcHist([image], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return hist.flatten()
